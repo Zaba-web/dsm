49c32ff66a80f6fcc9fc01467bfd0741fc11845f
Andriy <53307226+Zaba-web@users.noreply.github.com>
2020-09-06 18:50:36 +0300

    Improve rendedData function

--- src/scripts/InfoRenderer.js
+++ src/scripts/InfoRenderer.js
@@ -6,8 +6,14 @@ export function renderData(data, query){
         
         let prefix = element.dataset.prefix != undefined ? element.dataset.prefix : "";
         let suffix = element.dataset.suffix != undefined ? element.dataset.suffix : "";
+        let value = data[neededProperty];
 
-        element.innerHTML = prefix + data[neededProperty] + suffix;
+        if(element.dataset.sublevel != undefined){
+            element.innerHTML = prefix + value[0][element.dataset.sublevel] + suffix;
+        }else{
+            element.innerHTML = prefix + value + suffix;
+        }
+
     }
 }
 
@@ -25,7 +31,7 @@ export function renderCoreLoading(data){
     for(let i = 0; i < coresLoad.length; i++){
         let loadPercent = coresLoad[i].load.toFixed(2);
         if(firstTime){
-            coreContainer.innerHTML = coreContainer.innerHTML + `<div class="core"><h3 class='core_number'>${i+1}:</h3><div class="load"><div class="load-value" style='width:${loadPercent}%' id="core-number-${i}"></div></div><small id='core-number-${i}-loading-precent' class='core-load-percent'>${loadPercent}</small></div>`;
+            coreContainer.innerHTML = coreContainer.innerHTML + `<div class="core"><h3 class='core_number'>${i+1}:</h3><div class="load" style='width:160px'><div class="load-value" style='width:${loadPercent}%' id="core-number-${i}"></div></div><div class='core-load-percent' style='width:27px'><small id='core-number-${i}-loading-precent'>${loadPercent}</small></div></div>`;
         }else{
             let core = document.getElementById(`core-number-${i}`);
             core.style.width = loadPercent+"%";
@@ -35,11 +41,17 @@ export function renderCoreLoading(data){
     }
 }
 
-export function renderDisplayCount(count){
+export function renderDisplayCount(count, ipc){
     if(count > 1){
         let displayCountContainer = document.getElementById("display-selecter");
         for(let i = 0; i < count; i++){
-            displayCountContainer.innerHTML = displayCountContainer.innerHTML + `<span class='display h3-like subtitle' title='Pin to the ${i+1} display' data-number='display-${i}'>[D${i+1}]</span>`;
+            displayCountContainer.innerHTML = displayCountContainer.innerHTML + `<span class='display h3-like subtitle' title='Pin to the ${i+1} display' data-number='${i}'>[D${i+1}]</span>`;
+        }
+        let displayElements = document.getElementsByClassName("display");
+        for (let i = 0; i < displayElements.length; i++) {
+            displayElements[i].addEventListener('click', function(e){
+                ipc.send("change-display",e.target.dataset.number);
+            });
         }
     }
 }
